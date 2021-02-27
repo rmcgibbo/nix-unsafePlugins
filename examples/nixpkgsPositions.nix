a560db317c786c0bfc228fbffc38f2eb8ad94b5d
Robert T. McGibbon <rmcgibbo@gmail.com>
2021-02-27 13:52:54 -0500

    Add eval nixpkgs positions

---
+++ examples/nixpkgsPositions.nix
@@ -0,0 +1,28 @@
+let
+  pkgs = import <nixpkgs> {};
+  lib = pkgs.lib;
+
+  getPosition = v: v.meta.position or null;
+  isDrv = attrset: attrset ? "type" && attrset.type == "derivation";
+  shouldRecurse = attrset: !(isDrv attrset) && (attrset.recurseForDerivations or false);
+
+  tryMapAttrsRecursiveCond = cond: f: set:
+    let
+      recurse = path: set:
+        let
+          g =
+            name: value:
+            if builtins.isAttrs value && cond value
+              then recurse (path ++ [name]) value
+              else f (path ++ [name]) value;
+          tryG = name: value:
+            (builtins.unsafeSuperTryEval (g name value)).value or null;
+        in builtins.mapAttrs tryG set;
+    in recurse [] set;
+
+  recursiveFindFileAttrs = pkgSet: tryMapAttrsRecursiveCond
+    shouldRecurse
+    (name: value: if isDrv value then (getPosition value) else false)
+    pkgSet;
+in
+  recursiveFindFileAttrs pkgs
--- test.sh
+++ test.sh
@@ -5,3 +5,5 @@ args="--option plugin-files result/lib/nix/plugins/libunsafePlugins.so"
 nix eval $args '(builtins.unsafeTraceFd 3 "foo" "bar")' 3>&1
 
 nix eval  $args '(builtins.unsafeSuperTryEval (1+"a"))'
+
+nix eval $args --json -f ./examples/nixpkgsPositions.nix '' > /dev/null
